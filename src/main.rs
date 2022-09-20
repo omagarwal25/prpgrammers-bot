adfc8056fdfffc48e9e95c32dcf11c9f429c3110
omagarwal25 <lidagarwal@gmail.com>
2022-09-20 11:56:27 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use once_cell::sync::OnceCell;
+use poise::{serenity_prelude::{self as serenity, OnlineStatus, Activity}, Event::Ready};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+// User data, which is stored and accessible in all command invocations
+struct Data {}
+
+pub static CTX: OnceCell<serenity::Context> = OnceCell::new();
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+async fn event_listener(
+    ctx: &serenity::Context,
+    event: &poise::Event<'_>,
+    framework: poise::FrameworkContext<'_, Data, Error>,
+    _user_data: &Data,
+) -> Result<(), Error> {
+    match event {
+        poise::Event::Ready { data_about_bot } => {
+            println!("{} is ready", data_about_bot.user.name);
+            let _ = CTX
+                .set(ctx.to_owned())
+                .map_err(|_| println!("Global CTX already set"));
+            println!("Setting presence");
+            ctx.set_presence(
+                Some(Activity::playing(format!("water game"))),
+                OnlineStatus::Online,
+            )
+            .await;
+        }
+        poise::Event::ReactionAdd { add_reaction } => {
+            if add_reaction.emoji.unicode_eq("📌") {
+                let msg = add_reaction.message(&ctx).await?;
+                msg.pin(&ctx).await?;
+            }
+        }
+        poise::Event::ReactionRemove { removed_reaction } => {
+            if removed_reaction.emoji.unicode_eq("📌") {
+                if removed_reaction.message(&ctx).await?.reactions.iter().find(|r| r.reaction_type.unicode_eq("📌")).is_none() {
+                    removed_reaction.message(&ctx).await?.unpin(&ctx).await?;
+                }
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age(), register()],
+            listener: |ctx, event, framework, user_data| {
+                Box::pin(event_listener(ctx, event, framework, user_data))
+            },
+            ..Default::default()
+        })
+        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
+
+    framework.run().await.unwrap();
+}
